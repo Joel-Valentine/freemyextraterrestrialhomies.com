6195850b8bd26494e6cf41f4c214c11788e34255
WillCooter <will@cooters.co.uk>
2022-12-12 14:41:33 +0000

    further improved movement

--- pages/ufo.tsx
+++ pages/ufo.tsx
@@ -5,11 +5,18 @@ const ufoImage = "/ufo clipart surrounded by white background.png";
 const explosionImage = "/clipart explosion with white background.png";
 
 const UFO = () => {
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const screenWidth = window.innerWidth - 120;
+  const screenHeight = window.innerHeight - 60;
+  const windowWidth = window.document.documentElement.scrollWidth - 60;
+  const windowHeight = window.document.documentElement.scrollHeight - 60;
+
+  const [x, setX] = useState(Math.random() * screenWidth);
+  const [y, setY] = useState(Math.random() * screenHeight);
   const [dx, setDx] = useState(2);
   const [dy, setDy] = useState(1);
   const [imageUrl, setImageUrl] = useState(ufoImage);
+  const [outOfXBounds, setOutOfXBounds] = useState(false);
+  const [outOfYBounds, setOutOfYBounds] = useState(false);
 
   const explodeUfo = () => {
     setImageUrl(explosionImage);
@@ -21,19 +28,28 @@ const UFO = () => {
       return;
     }
 
-    const screenWidth = window.innerWidth - 120;
-    const screenHeight = window.document.documentElement.scrollHeight - 60;
-
     const move = setInterval(() => {
       if (x + dx > screenWidth || x + dx < 0) {
-        setDx(-dx);
+        if (!outOfXBounds) {
+          setDx(-dx);
+          setOutOfXBounds(true);
+        }
+      } else {
+        setOutOfXBounds(false);
       }
-      if (y + dy > screenHeight || y + dy < 0) {
-        setDy(-dy);
+
+      if (y + dy > windowHeight || y + dy < 0) {
+        if (!outOfYBounds) {
+          setDy(-dy);
+          setOutOfYBounds(true);
+        }
+      } else {
+        setOutOfYBounds(false);
       }
+
       setX(x + dx + Math.sin(y / 200));
       setY(y + dy + Math.cos(x / 200));
-    }, 10);
+    }, 5);
 
     return () => clearInterval(move);
   });
