d9a05859a842dc45b40d21bf8a2f8fe8f9d20387
WillCooter <will@cooters.co.uk>
2022-12-18 19:59:11 +0000

    test 7

--- pages/api/socketio.ts
+++ pages/api/socketio.ts
@@ -25,6 +25,10 @@ const handle = async (req: NextApiRequest, res: NextApiResponseServerIO) => {
 
   if (!res.socket.server.io) {
     console.log("New Socket.io server...");
+    console.log("socket...");
+    console.log(res.socket);
+    console.log("server...");
+    console.log(res.socket.server);
     // adapt Next's net Server to http Server
     const httpServer: NetServer = res.socket.server as any;
     const io = new ServerIO(httpServer, {
@@ -32,6 +36,12 @@ const handle = async (req: NextApiRequest, res: NextApiResponseServerIO) => {
     });
     // append SocketIO server to Next.js socket server response
     res.socket.server.io = io;
+  } else {
+    console.log("else...");
+    console.log("socket...");
+    console.log(res.socket);
+    console.log("server...");
+    console.log(res.socket.server);
   }
   res.end();
 };
